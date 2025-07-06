ae6138c7927b2696b62dc435b811cb144dc621e3
ChardonBleu <f.rondi@orange.fr>
2025-07-06 16:12:23 +0200

    ✨ feat: manage qpi formating

--- app/api/fetchUser.ts
+++ app/api/fetchUser.ts
@@ -1,5 +1,11 @@
 export async function fetchUser(id: string) {
   const res = await fetch(`http://localhost:3000/user/${id}`);
   const data = await res.json();
-  return data.data;
+  const userScore = data.data.score ? data.data.score : data.data.todayScore ? data.data.todayScore : 0
+  return {
+    id: data.data.id,
+    userInfos: data.data.userInfos,
+    score: userScore,
+    keyData: data.data.keyData,
+  }
 }
