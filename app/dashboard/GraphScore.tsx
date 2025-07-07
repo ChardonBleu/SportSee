cfd8cc358622699d98426348b5d3ac82e9fca0fc
ChardonBleu <f.rondi@orange.fr>
2025-07-07 07:33:20 +0200

    💄 style: add style to graph score

--- app/dashboard/GraphScore.tsx
+++ app/dashboard/GraphScore.tsx
@@ -3,11 +3,14 @@
 export function GraphScore({ score }: { score: number }) {
   return (
     <>
-      <section className="bg-dust rounded-md w-1/3 ">
-        <p className="font-medium text-base pt-8 pl-8">Score</p>
-        <p className="flex items-center justify-center text-5xl">
-          {score * 100}%
-        </p>
+      <section className="bg-dust rounded-md w-1/3 font-medium text-base">
+        <p className=" pt-8 pl-8">Score</p>
+        <div className="flex flex-col justify-center items-center">
+          <p className=" text-5xl">
+            {score * 100}%
+          </p>
+          <p className="text-ashes">de votre<br/>objectif</p>
+        </div>
       </section>
     </>
   );
